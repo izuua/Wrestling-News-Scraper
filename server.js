f989e07c387dfca399ca9a78aa5b31d2df35486a
izuua <izuuaaf@gmail.com>
2019-08-27 21:58:54 -0400

    Added the axios call to scrape information from lordsofpain.net.

--- server.js
+++ server.js
@@ -31,6 +31,25 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/lopdb";
 
 mongoose.connect(MONGODB_URI);
 
+axios.get("https://lordsofpain.net/").then(function(response) {
+
+    var $ = cheerio.load(response.data);
+
+    var results = [];
+
+    $("h4").each(function(i, element) {
+        var title = $(element).children().text();
+        var link = $(element).find("a").attr("href");
+
+        results.push({
+            title: title,
+            link: link
+        })
+    })
+
+    console.log(results);
+})
+
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
