8bca5ab1efc2d75645495135d5de8c999ccac9e0
izuua <izuuaaf@gmail.com>
2019-08-27 23:33:08 -0400

    Added a basic card to the html layout.

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ console.log("app.js loaded");
 $(document).on("click", "#news-load", () => {
     $.getJSON("/articles", function(data) {
         data.forEach(function(element) {
-            $("#wrapper").append(`<a href = "${element.link}"><p data-id="${element._id}"> ${element.title} <br /></p></a>`)
+            $("#card-news").append(`<a href = "${element.link}"><p data-id="${element._id}"> ${element.title} <br /></p></a>`)
         })
     })
 });
